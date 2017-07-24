bbe3d17f6c5b9be32e85bbff2d24a3c6d9034a92
sainthkh <sainthkh@naver.com>
2017-07-24 12:18:35 +0900

    Added not implemented yet notice.

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,3 +1,13 @@
 <div class="page-header">
-  <h1>{!! App\title() !!}</h1>
+  <div class="card">
+    <div class="card-header deep-orange accent-3 white-text">
+        Notice
+    </div>
+    <div class="card-block">
+      <h4 class="card-title">This page will contain "{!! App\title() !!}" content.</h4>
+      <p class="card-text">This page is not created yet. I filled the page with this because I want to understand more about you.</p>
+      <p class="card-text">I hope you like this.</p>
+      <p class="card-text">If you don't, let's talk more about the page and find the best answer.</p>
+    </div>
+  </div>
 </div>
